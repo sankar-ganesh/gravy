d1d6f8820f4ce7d81a3a5dff9fbe0bf622f16e85
Sankar Ganesh <sankarg@freshdesk.com>
2018-01-27 09:30:10 +0530

    cyclic object with serializer

---
+++ javascript/examples/cyclic.js
@@ -0,0 +1,19 @@
+var data = {
+  firstName: 'Sankar Ganesh',
+  lastName: 'Arumugamangalam Nellaivinayagam'
+};
+
+JSON.stringify(data) // prints "{"firstName":"Sankar Ganesh","lastName":"Arumugamangalam Nellaivinayagam"}"
+
+data.next = data;    // Add cyclic
+
+JSON.stringify(data) // Uncaught TypeError: Converting circular structure to JSON
+
+JSON.toJSON = function() {    // Define Serializer
+  return {
+    firstName: this.firstName,
+    lastName: this.lastName
+  };
+};
+
+JSON.stringify(data) // prints "{"firstName":"Sankar Ganesh","lastName":"Arumugamangalam Nellaivinayagam"}"
