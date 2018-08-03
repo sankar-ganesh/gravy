71450615e4c02b5be3596995153fc62685178967
Sankar Ganesh <sankarg@freshdesk.com>
2018-08-03 21:45:09 +0530

    Adding unsubscribe function to the class

--- javascript/examples/pubsub.js
+++ javascript/examples/pubsub.js
@@ -18,21 +18,38 @@ var PubSubClass = (function() {
 	}
 
 	PubSub.prototype.subscribe = function(event, callback) {
-		// Setup Event
-		if (!events.hasOwnProperty(event)) {
-			events[event] = [];
-		}
+		if (event && callback) {
+			// Setup Event
+			if (!events.hasOwnProperty(event)) {
+				events[event] = [];
+			}
 
-		// Attach Event Callback
-		events[event].push(callback);
+			// Attach Event Callback
+			events[event].push(callback);
+		}
 	};
 
 	PubSub.prototype.publish = function(event, data) {
-		// Broadcast Event Data
-		if (events.hasOwnProperty(event)) {
-			events[event].map(callback => callback(data));
+		if (event) {
+			// Broadcast Event Data
+			if (events.hasOwnProperty(event)) {
+				events[event].map(callback => callback(data));
+			}
 		}
 	};
+
+	PubSub.prototype.unsubscribe = function(event, callback) {
+		if (event && callback) {
+			// Identify Event
+			if (events.hasOwnProperty(event)) {
+				for (let i = events[event].length - 1; i >= 0; i--) {
+					if (events[event][i].prototype === callback.prototype) {
+						events[event].splice(i, 1);
+					};
+				}
+			}
+		}
+	}
 
 	PubSub.prototype.getInstance = function() {
 		return instance || new PubSub();
