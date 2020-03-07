6764b00b1410cd83eac1e0b9cfe39a2464a7cdf9
Sankar Ganesh <sankarg@freshdesk.com>
2020-03-07 15:21:50 +0530

    Fixing typo in cyclic code snippet

--- javascript/examples/cyclic.js
+++ javascript/examples/cyclic.js
@@ -9,7 +9,7 @@ data.next = data;    // Add cyclic
 
 JSON.stringify(data) // Uncaught TypeError: Converting circular structure to JSON
 
-JSON.toJSON = function() {    // Define Serializer
+data.toJSON = function() {    // Define Serializer
   return {
     firstName: this.firstName,
     lastName: this.lastName
